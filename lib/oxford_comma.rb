cb75633a58b8ffb5295da8aa6cea1a83e827e559
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-09-12 02:54:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  newArray = []
+  if array.length == 1 
+    array.join("")
+  elsif array.length == 2
+    array.join(" and ")
+  else 
+    array[0..-2].join(", ") + ", and " + array.last
+  end
 end
